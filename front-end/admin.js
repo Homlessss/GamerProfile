7c4700389934e916d2a8dffc369ead7c74c12f32
NguyenHien288 <49906980+NguyenHien288@users.noreply.github.com>
2019-11-02 20:59:09 +0700

    admin page

--- front-end/admin.js
+++ front-end/admin.js
@@ -1,7 +1,5 @@
 var n = 0;
 var id = new Array();
-var btn_id;
-var btn_id2;
 
 $(document).ready(function() {
   var $loading = $(".spinner-border").hide();
@@ -25,19 +23,19 @@ $(document).ready(function() {
           id[i] = data.data[i]._id;
 
           $("#list-content").append(`
-            <tr class="row  all-content" id="row${i}">
+            <tr class="row  all-content" id="row${id[i]}">
 
-            <td class="col-lg-3" id="nameData">${data.data[i].name}</td>
-            <td class="col-lg-2" id="teamData">${data.data[i].team}</td>
-            <td class="col-lg-2" id="nationalityData">${data.data[i].nationality}</td>
-            <td class="col-lg-3" id="imageData"><img src="${data.data[i].image}" alt="Avatar" height="90"></td>
-            <td class="col-lg-1" id="laneData">${data.data[i].lane}</td>
+            <td class="col-lg-3" id="nameData${id[i]}">${data.data[i].name}</td>
+            <td class="col-lg-2" id="teamData${id[i]}">${data.data[i].team}</td>
+            <td class="col-lg-2" id="nationalityData${id[i]}">${data.data[i].nationality}</td>
+            <td class="col-lg-3" id="imageData${id[i]}"><img src="${data.data[i].image}" alt="Avatar" height="90"></td>
+            <td class="col-lg-1" id="laneData${id[i]}">${data.data[i].lane}</td>
             <td class="col-lg-1" id="td-btn">
-              <button class="btn btn-outline-secondary btn-edit" id="btn-edit${i}" onclick="editData(this.id)"
+              <button class="btn btn-outline-secondary btn-edit" id="btn-edit${id[i]}" onclick="editData('${id[i]}')"
               data-toggle="modal"
               data-target="#editModal">Edit</button>
 
-              <button class="btn btn-outline-secondary btn-delete" id="btn-delete${i}" onclick="deleteData(this.id)" 
+              <button class="btn btn-outline-secondary btn-delete" id="btn-delete${id[i]}" onclick="deleteData('${id[i]}')" 
               data-toggle="modal"
               data-target="#deleteModal">Delete</button>
             </td>
@@ -73,106 +71,135 @@ $(document).ready(function() {
           image: $("#add-img").val()
         }),
         success: function(data) {
-          console.log(data);
+          console.log("POST", data);
+          id.push(data.data._id);
+          $("#list-content").append(`
+            <tr class="row all-content" id="row${data.data._id}">
+            <td class="col-lg-3" id="nameData${data.data._id}">${$(
+            "#add-name"
+          ).val()}</td>
+            <td class="col-lg-2" id="teamData${data.data._id}">${$(
+            "#add-team"
+          ).val()}</td>
+            <td class="col-lg-2" id="nationalityData${data.data._id}">${$(
+            "#add-nationality"
+          ).val()}</td>
+            <td class="col-lg-3" id="imageData${data.data._id}"><img src="${$(
+            "#add-img"
+          ).val()}" alt="Avatar" height="90"></td>
+            <td class="col-lg-1" id="laneData${data.data._id}">${$(
+            "#add-lane"
+          ).val()}</td>
+            <td class="col-lg-1 " id="td-btn">
+              <button class="btn btn-outline-secondary btn-edit" id="btn-edit${
+                data.data._id
+              }" onclick="editData('${data.data._id}')"
+              data-toggle="modal"
+              data-target="#editModal">Edit</button>
+
+              <button class="btn btn-outline-secondary btn-delete" id="btn-delete${
+                data.data._id
+              }" onclick="deleteData('${data.data._id}')"
+              data-toggle="modal"
+              data-target="#deleteModal"
+              >Delete</button>
+            </td>
+            </tr>
+            `);
+          n++;
         },
         error: function(err) {
           console.log("e", err);
         }
       });
-      $("#list-content").append(`
-            <tr class="row all-content" id="row${n}">
-            <td class="col-lg-3 hover-data">${$("#add-name").val()}</td>
-            <td class="col-lg-2">${$("#add-team").val()}</td>
-            <td class="col-lg-2">${$("#add-nationality").val()}</td>
-            <td class="col-lg-3"><img src="${$(
-              "#add-img"
-            ).val()}" alt="Avatar" height="90"></td>
-            <td class="col-lg-1">${$("#add-lane").val()}</td>
-            <td class="col-lg-1 " id="td-btn">
-              <button class="btn btn-outline-secondary btn-edit" id="btn-edit${n}" onclick="editData(this.id)"
-              data-toggle="modal"
-              data-target="#editModal">Edit</button>
-
-              <button class="btn btn-outline-secondary btn-delete" id="btn-delete${n}" onclick="deleteData(this.id)"
-              data-toggle="modal"
-              data-target="#deleteModal">Delete</button>
-            </td>
-            </tr>
-            `);
       n++;
       document.elementFromPoint(0, 0).click();
     }
   });
+
+  $("#editModal").on("show.bs.modal", function(event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data("whatever"); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find(".modal-title").text("Edit");
+  });
 });
+var btn_id;
+var btn_id2;
+var selectedId;
+
 function deleteData(btn_id) {
-  for (let i = 0; i < n; i++) {
-    if (btn_id === `btn-delete${i}`) {
-      $("#deleteConf").click(function() {
-        {
-          $.ajax({
-            type: "DELETE",
-            url: `http://localhost:3000/api/player/${id[i]}`,
-            contentType: "application/json",
-            success: console.log("done"),
-            error: console.log("e")
-          });
-          $("tr").remove(`#row${i}`);
-          document.elementFromPoint(0, 0).click();
-        }
-      });
-    }
-  }
+  selectedId = btn_id;
+  btn_id = 0;
 }
+
+function deleteFunc(id) {
+  if (!id) return;
+  $.ajax({
+    type: "DELETE",
+    url: `http://localhost:3000/api/player/${id}`,
+    contentType: "application/json",
+    success: function(data) {
+      $("tr").remove(`#row${id}`);
+    },
+    error: console.log("e", error)
+  });
+  document.elementFromPoint(0, 0).click();
+}
+
 function editData(btn_id2) {
-  for (let i = 0; i < n; i++) {
-    if (btn_id2 === `btn-edit${i}`) {
-      $("#updatePlayer").click(function() {
-        console.log(`${id[i]}`);
-        $.ajax({
-          type: "PUT",
-          url: `http://localhost:3000/api/player/${id[i]}`,
-          contentType: "application/json",
-          data: JSON.stringify({
-            name: $("#update-name").val(),
-            team: $("#update-team").val(),
-            nationality: $("#update-nationality").val(),
-            lane: $("#update-lane").val(),
-            image: $("#update-img").val()
-          }),
-          success: function(data) {
-            console.log(data);
-          },
-          error: function(err) {
-            console.log("e", err);
-          }
-        });
-        if ($("#update-name").val()) {
-          document.getElementById("nameData").innerHTML = $(
-            "#update-name"
-          ).val();
-        }
-        if ($("#update-team").val()) {
-          document.getElementById("teamData").innerHTML = $(
-            "#update-team"
-          ).val();
-        }
-        if ($("#update-nationality").val()) {
-          document.getElementById("nationalityData").innerHTML = $(
-            "#update-nationality"
-          ).val();
-        }
-        if ($("#update-lane").val()) {
-          document.getElementById("laneData").innerHTML = $(
-            "#update-lane"
-          ).val();
-        }
-        if ($("#update-img").val()) {
-          document.getElementById("imageData").innerHTML = $(
-            "#update-img"
-          ).val();
-        }
-        document.elementFromPoint(0, 0).click();
-      });
+  selectedId = btn_id2;
+  btn_id2 = 0;
+}
+
+function updateData(id) {
+  if (!id) return;
+  $.ajax({
+    type: "PUT",
+    url: `http://localhost:3000/api/player/${id}`,
+    contentType: "application/json",
+    data: JSON.stringify({
+      name: $("#update-name").val(),
+      team: $("#update-team").val(),
+      nationality: $("#update-nationality").val(),
+      lane: $("#update-lane").val(),
+      image: $("#update-img").val()
+    }),
+    success: function(data) {
+      if ($("#update-name").val()) {
+        document.getElementById(`nameData${id}`).innerHTML = $(
+          "#update-name"
+        ).val();
+      }
+      if ($("#update-team").val()) {
+        document.getElementById(`teamData${id}`).innerHTML = $(
+          "#update-team"
+        ).val();
+      }
+      if ($("#update-nationality").val()) {
+        document.getElementById(`nationalityData${id}`).innerHTML = $(
+          "#update-nationality"
+        ).val();
+      }
+      if ($("#update-lane").val()) {
+        document.getElementById(`laneData${id}`).innerHTML = $(
+          "#update-lane"
+        ).val();
+      }
+      if ($("#update-img").val()) {
+        document.getElementById(`imageData${id}`).innerHTML = $(
+          "#update-img"
+        ).val();
+      }
+      document.elementFromPoint(0, 0).click();
+      selectedId = undefined;
+    },
+    error: function(err) {
+      console.log("e", err);
+      document.elementFromPoint(0, 0).click();
+      selectedId = undefined;
     }
-  }
+  });
 }

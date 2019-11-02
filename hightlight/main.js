4684d92c976c8385bda889d8d6db67ef172e4895
Homlessss <mkhai2308@gmail.com>
2019-11-02 20:59:21 +0700

    update

--- hightlight/main.js
+++ hightlight/main.js
@@ -22,16 +22,29 @@ $(document).ready(function(){
               const appendedElement = `
                 <div class="col-12 video-container">
                   <div class="row">
-                    <div class="embed-responsive embed-responsive-16by9 col-6 col-md-6">
+                    <div class="embed-responsive embed-responsive-16by9 col-6 col-md-7 mt-2 ml-0">
                       <iframe src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <div class="col-6 col-md-12">
-                        <h2 class="video-title">${item.snippet && item.snippet.title}</h2>
+                    <div class="row-chid col-6 col-md-5">
+                      <h2 class="video-title">${item.snippet && item.snippet.title}</h2>
                       <p class="video-description">${item.snippet && item.snippet.description}</p>
                     </div>
                   <div>
                 </div>
               `
+              $(window).resize(function(){
+ 
+                var width = $(window).width();
+                console.log(width);
+                if (width <= 1000){
+                  $('.row-chid').removeClass('col-6 col-md-5');
+                  $('.row-chid').addClass('col-6, col-md-12');
+                }
+                else{
+                  $('.row-chid').removeClass('col-6, col-md-12');
+                  $('.row-chid').addClass('col-6 col-md-5');
+                }
+            });
               $('.main-body').append(appendedElement);
             })
           }

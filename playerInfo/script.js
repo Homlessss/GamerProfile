32e25d4bb9e3f55dd059fba1202c1bedf68b6db6
NguyenHien288 <49906980+NguyenHien288@users.noreply.github.com>
2019-11-04 20:55:07 +0700

    list

--- playerInfo/script.js
+++ playerInfo/script.js
@@ -23,30 +23,39 @@ function getTeam(inputVal) {
       console.log(data);
       if (data && data.data) {
         for (var i = 0; i < data.data.length; i++) {
+          strName = data.data[i].name;
+          str1 = strName.slice(0, 1);
+          str2 = strName.slice(1);
+          strName = str1.toUpperCase() + str2;
+
+          strTeam = data.data[i].team;
+          str1 = strTeam.slice(0, 1);
+          str2 = strTeam.slice(1);
+          strTeam = str1.toUpperCase() + str2;
+
+          strLane = data.data[i].name;
+          str1 = strLane.slice(0, 1);
+          str2 = strLane.slice(1);
+          strLane = str1.toUpperCase() + str2;
+
+          strNationality = data.data[i].name;
+          str1 = strNationality.slice(0, 1);
+          str2 = strNationality.slice(1);
+          strNationality = str1.toUpperCase() + str2;
           if (data.data[i].team === inputVal) {
             $("#player-list").append(`
-                      <div class="col-12 mt-3 mb-3">
-                          <div class="row">
-                              <div class="col-3">
-                                  <h3 class="text-success">${data.data[i].name}</h3>
-                                  <img class="image-placeholder" src="${data.data[i].image} "></img>
-                                  // them src img
-                                  <h4>Background Information</h4>
-                                  <table>
-                                      <tr>
-                                          <th>Team</th>
-                                          <td>${data.data[i].team}</td>
-                                      </tr>
-                                      <tr>
-                                          <th>Natiionality</th>
-                                          <td>${data.data[i].nationality}</td>
-                                      </tr>
-                              </div>
-                              <div class="col-9">
-                                  <h3>Lane</h3>
-                                  <p>${data.data[i].lane}</p>
-                              </div>
-                          </div>
+            <div class="content">
+            <div class="info">
+              <p id="infoName">${strName}</p>
+              <p id="infoLane">${strLane}</p>
+              <p id="infoTeam">Team: ${strTeam}</p>
+              <p id="infoNation">Nationality: ${strNationality}</p>
+              
+            </div>
+            <div class="imgContent">
+              <img class="image-placeholder imgProfile" src="${data.data[i].image}"></img>
+            </div>
+          </div>
                           `);
           }
         }
@@ -68,31 +77,40 @@ function getPlayer(inputVal) {
       console.log(data);
       if (data && data.data) {
         for (var i = 0; i < data.data.length; i++) {
+          strName = data.data[i].name;
+          str1 = strName.slice(0, 1);
+          str2 = strName.slice(1);
+          strName = str1.toUpperCase() + str2;
+
+          strTeam = data.data[i].team;
+          str1 = strTeam.slice(0, 1);
+          str2 = strTeam.slice(1);
+          strTeam = str1.toUpperCase() + str2;
+
+          strLane = data.data[i].name;
+          str1 = strLane.slice(0, 1);
+          str2 = strLane.slice(1);
+          strLane = str1.toUpperCase() + str2;
+
+          strNationality = data.data[i].name;
+          str1 = strNationality.slice(0, 1);
+          str2 = strNationality.slice(1);
+          strNationality = str1.toUpperCase() + str2;
           if (data.data[i].name === inputVal) {
             $("#player-list").append(`
-                      <div class="col-12 mt-3 mb-3">
-                          <div class="row">
-                              <div class="col-3">
-                                  <h3 class="text-success">${data.data[i].name}</h3>
-                                  <img class="image-placeholder" src="${data.data[i].image}"></img>
-                                  // them src img
-                                  <h4>Background Information</h4>
-                                  <table>
-                                      <tr>
-                                          <th>Team</th>
-                                          <td>${data.data[i].team}</td>
-                                      </tr>
-                                      <tr>
-                                          <th>Nationality:</th>
-                                          <td>${data.data[i].nationality}</td>
-                                      </tr>
-                              </div>
-                              <div class="col-9">
-                                  <h3>Lane</h3>
-                                  <p>${data.data[i].lane}</p>
-                              </div>
-                          </div>
-                          `);
+          <div class="content">
+                <div class="info">
+                  <p id="infoName">${strName}</p>
+                  <p id="infoLane">${strLane}</p>
+                  <p id="infoTeam">Team: ${strTeam}</p>
+                  <p id="infoNation">Nationality: ${strNationality}</p>
+                  
+                </div>
+                <div class="imgContent">
+                  <img class="image-placeholder imgProfile" src="${data.data[i].image}"></img>
+                </div>
+              </div>
+             `);
           }
         }
       }

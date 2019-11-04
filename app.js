48d84657772f6c6a6c3ebb4dc753213045adb48b
Homlessss <mkhai2308@gmail.com>
2019-11-04 22:12:19 +0700

    update

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ const authRouter = require("./Server/api/module/auth/auth.router");
 const teamRouter = require("./Server/api/module/teams/team.router");
 
 mongoose.connect(config.mongoConnectionString);
-// const PORT = 3000;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/", express.static("../client"));
+app.use("/", express.static('front-end'));
+app.use('/highlight', express.static('highlight'));
+app.use('/playerInfo', express.static('playerInfo'));
+app.use('/logo-image', express.static('logo-image'));
 app.use("/api/player", playerRouter);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/team", teamRouter);
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log(`Server listening in ${PORT}`);
+  console.log(`Server listening in 3000`);
 });

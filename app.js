d0110940451bd781929d7dca47e63b2011e6fffa
NguyenHien288 <49906980+NguyenHien288@users.noreply.github.com>
2019-11-06 14:29:01 +0700

    a

--- app.js
+++ app.js
@@ -9,20 +9,16 @@ const authRouter = require("./Server/api/module/auth/auth.router");
 const teamRouter = require("./Server/api/module/teams/team.router");
 
 mongoose.connect(config.mongoConnectionString);
-<<<<<<< HEAD
-const PORT = 3000;
-=======
 
-const port = process.env.PORT || 3000
->>>>>>> 7ce0c81d7bd35e13e915043eb636dec87ba4604f
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/", express.static('front-end'));
-app.use('/highlight', express.static('highlight'));
-app.use('/playerInfo', express.static('playerInfo'));
-app.use('/logo-image', express.static('logo-image'));
+app.use("/", express.static("front-end"));
+app.use("/highlight", express.static("highlight"));
+app.use("/playerInfo", express.static("playerInfo"));
+app.use("/logo-image", express.static("logo-image"));
 app.use("/api/player", playerRouter);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
@@ -31,4 +27,3 @@ app.use("/api/team", teamRouter);
 app.listen(port, function() {
   console.log(`Server listening in` + port);
 });
-
